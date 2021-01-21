8f60014ffd45ed1ad628d239ce7406f2bd0fbe48
klinsmann lopes <lopesklismann@gmail.com>
2021-01-21 19:18:20 -0300

    Checking user in the data structure

--- lib/account_simulator/cli/login/account_consult.ex
+++ lib/account_simulator/cli/login/account_consult.ex
@@ -1,7 +1,10 @@
 defmodule AccountSimulator.CLI.Login.AccountConsult do
   
-  def get_balance(usuarios, usuario) do
+  def get_balance(usuario, usuarios) do
     IO.puts usuarios[usuario] |> Enum.map_join("\n", fn {k, v} -> "#{k}:#{v}" end)
   end
 
+  def get_user?(usuario, usuarios) do
+    Keyword.fetch(usuarios, usuario) 
+  end
 end
