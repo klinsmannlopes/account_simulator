5dcfa4847884b4050d4d1f9cc25895ab57a40c76
klinsmann lopes <lopesklismann@gmail.com>
2021-01-21 17:52:29 -0300

    Setting data that will go into the login menu

---
+++ lib/account_simulator/cli/menu/itens_login.ex
@@ -0,0 +1,9 @@
+defmodule AccountSimulator.CLI.Menu.ItensLogin do
+  alias AccountSimulator.CLI.Menu.Menu
+
+  def all,
+    do: [
+      %Menu{label: "Login", id: :login},
+      %Menu{label: "Criar um cadastro", id: :create},
+    ]
+end
