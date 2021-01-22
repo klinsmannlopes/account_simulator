f5133414a1cb5867a180066f7e2d0c6e3c125553
klinsmann lopes <lopesklismann@gmail.com>
2021-01-22 00:29:57 -0300

    Refactor comments

--- lib/account_simulator/cli/login/account_consult.ex
+++ lib/account_simulator/cli/login/account_consult.ex
@@ -1,33 +1,8 @@
 defmodule AccountSimulator.CLI.Login.AccountConsult do
-  alias Mix.Shell.IO, as: Shell
 
   @moduledoc """
   Realiza querys na estrutura de dados.
   """
-
-  @doc """
-  Obtem a atual quantia de dinheiro do usuário na estrutura de dados.
-
-  ## Parâmetros
-
-    - usuarios: Lista que representa a estrutura de dados -> Já estar sendo passado uma estrutura com dados padrões.
-
-    - usuario: Atom que representa o nome do usuário.
-
-  ## Exemplo 
-
-      iex> Consulta.get_balance(:klinsmann, usuarios)
-      AED:0
-      AFN:0
-      ...
-
-  """
-
-  def get_balance(usuario, usuarios) do
-    Keyword.get(usuarios, usuario)
-    |> Enum.map_join("\n", fn {k, v} -> "#{k}:#{v}" end)
-    |> Shell.info()
-  end
 
   @doc """
   É feita uma verificação na estrutura de dados para confirmar se o usuário existe.
