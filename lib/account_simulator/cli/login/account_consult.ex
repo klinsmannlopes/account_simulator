a530357ef7f1129ca8e877b86257a24a851c74e5
klinsmann lopes <lopesklismann@gmail.com>
2021-01-21 22:59:05 -0300

    Add comments in module

--- lib/account_simulator/cli/login/account_consult.ex
+++ lib/account_simulator/cli/login/account_consult.ex
@@ -1,10 +1,47 @@
 defmodule AccountSimulator.CLI.Login.AccountConsult do
+  alias Mix.Shell.IO, as: Shell
+  @moduledoc """
+  Realiza querys na estrutura de dados.
+  """
+
+  @doc """
+  Obtem a atual quantia de dinheiro do usuário na estrutura de dados.
+
+  ## Parâmetros
+
+    - usuarios: Lista que representa a estrutura de dados -> Já estar sendo passado uma estrutura com dados padrões.
+
+    - usuario: Atom que representa o nome do usuário.
+
+  ## Exemplo 
+  
+      iex> Consulta.get_balance(:klinsmann, usuarios)
+      AED:0
+      AFN:0
+      ...
+
+  """
   
   def get_balance(usuario, usuarios) do
-    IO.puts usuarios[usuario] |> Enum.map_join("\n", fn {k, v} -> "#{k}:#{v}" end)
+    Keyword.get(usuarios, usuario)
+    |> Enum.map_join("\n", fn {k, v} -> "#{k}:#{v}" end)
+    |> Shell.info()
   end
 
+  @doc """
+  É feita uma verificação na estrutura de dados para confirmar se o usuário existe.
+
+  ## Exemplo
+
+      iex> Consulta.get_user?([john: 104, stone: 220], :rita)
+      :error
+
+      iex> Consulta.get_user?([john: 233, stone: 10], :stone) 
+      {:ok, 10}
+      
+  """
+
   def get_user?(usuario, usuarios) do
-    Keyword.fetch(usuarios, usuario) 
+    Keyword.fetch(usuarios, usuario)
   end
 end
