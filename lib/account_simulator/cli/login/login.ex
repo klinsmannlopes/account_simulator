c6682ccd29cb750e3376bdee43cfa4e5fc058d1a
klinsmann lopes <lopesklismann@gmail.com>
2021-01-21 23:37:13 -0300

    Send user and list users for function

--- lib/account_simulator/cli/login/login.ex
+++ lib/account_simulator/cli/login/login.ex
@@ -6,8 +6,8 @@ defmodule AccountSimulator.CLI.Login.Login do
   alias Mix.Shell.IO, as: Shell
   alias AccountSimulator.Mix.Tasks.Utils.PromptHelper
   alias AccountSimulator.CLI.Login.AccountConsult
+  alias AccountSimulator.CLI.Menu.ChoiceTransactions
   alias AccountSimulator.CLI.Currency
-
 
   def login do
     PromptHelper.prompt_message("Digite seu nome de usuário: ")
@@ -15,20 +15,20 @@ defmodule AccountSimulator.CLI.Login.Login do
     |> access(standard_user())
   end
 
-  def access(usuario,usuarios) do
+  def access(usuario, usuarios) do
     case AccountConsult.get_user?(usuario, usuarios) do
       :error ->
         Shell.cmd("clear")
         Shell.error("Usuário não existe!")
         Shell.prompt("Pressione ENTER para voltar a tela de login...")
-        AccountSimulator.CLI.Menu.Choice.start()
+        AccountSimulator.CLI.Menu.ChoiceLogin.start()
+
       _ ->
-        Scribe.print(usuario, style: Scribe.Style.GithubMarkdown)
+        ChoiceTransactions.option_transactions(usuario, usuarios)
     end
   end
 
   def standard_user do
-    [maria: Currency.new(), stone: Currency.new(), john: Currency.new()]
+    [maria: Currency.new(), stone: Currency.new(), klinsmann: Currency.new()]
   end
-
 end
