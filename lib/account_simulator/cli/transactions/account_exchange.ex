976fa08b6fd954c6f68d9ff3f0eed6bdfc8857cf
klinsmann lopes <lopesklismann@gmail.com>
2021-02-01 11:05:11 -0300

    using exception for amount check

--- lib/account_simulator/cli/transactions/account_exchange.ex
+++ lib/account_simulator/cli/transactions/account_exchange.ex
@@ -1,10 +1,12 @@
 defmodule AccountSimulator.Mix.CLI.Trasactions.AccountExchange do
+  alias AccountSimulator.Mix.CLI.Errors.Exception
+
   def remove_currency(users, user, currency, value) do
     put_in (users[user])[currency], (users[user])[currency] - value
   end
 
   def add_currency(users, user, currency, value) do
-    #Critico.montante?(value)
+    Exception.amount?(value)
     put_in (users[user])[currency], (users[user])[currency] + value
   end
 end
