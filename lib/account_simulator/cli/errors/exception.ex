1f7cfba776ffb945f27a397bc3e25e08fa14ac34
klinsmann lopes <lopesklismann@gmail.com>
2021-02-01 11:07:50 -0300

    add comments

--- lib/account_simulator/cli/errors/exception.ex
+++ lib/account_simulator/cli/errors/exception.ex
@@ -1,4 +1,6 @@
 defmodule AccountSimulator.Mix.CLI.Errors.Exception do
+  
+  # Verificando valor passado.
   def amount(value) do
     unless value > 0 do
       PromptHelper.prompt_message("Dinheiro insuficiente")
--- lib/account_simulator/cli/transactions/account_exchange.ex
+++ lib/account_simulator/cli/transactions/account_exchange.ex
@@ -1,10 +1,12 @@
 defmodule AccountSimulator.Mix.CLI.Trasactions.AccountExchange do
   alias AccountSimulator.Mix.CLI.Errors.Exception
 
+  # Removendo dinheiro de uma moeda específica.
   def remove_currency(users, user, currency, value) do
     put_in (users[user])[currency], (users[user])[currency] - value
   end
 
+  # Adicionando dinheiro de uma moeda específica.
   def add_currency(users, user, currency, value) do
     Exception.amount?(value)
     put_in (users[user])[currency], (users[user])[currency] + value
