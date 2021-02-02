56de1c4109afcc35dacaa89430695db034cb0428
klinsmann lopes <lopesklismann@gmail.com>
2021-02-02 00:58:01 -0300

    Add news comments

--- lib/account_simulator/cli/transactions/account_exchange.ex
+++ lib/account_simulator/cli/transactions/account_exchange.ex
@@ -14,6 +14,7 @@ defmodule AccountSimulator.Mix.CLI.Trasactions.AccountExchange do
     put_in (users[user])[currency], (users[user])[currency] + value
   end
 
+  # Fluxo para relializar o câmbio das moedas. 
   def currency_change(users, user) do
     currency_input = get_currency_console(users, user)
     print_currency(currency_input, "first")
@@ -28,21 +29,24 @@ defmodule AccountSimulator.Mix.CLI.Trasactions.AccountExchange do
     
   end
 
+  # Verifica saldo e pega moeda digitada.
   def get_currency_console(users, user) do
-    AccountTransactions.verify_money?(users, user)
     currency = AccountTransactions.currency(user, users)
     AccountTransactions.check_balance_currency(currency, users, user)
     currency
   end
 
+  # Monstrando para usuário a primeira moeda.
   defp print_currency(currency, "first") do
     Shell.info("Primeira moeda: #{currency}.")
   end
 
+  # Monstrando para usuário a segunda moeda.
   defp print_currency(currency, "second") do
     Shell.info("Segunda moeda: #{currency}.")
   end
 
+  # Checando se as moedas são as mesmas.
   def check_currency_exchange?(currency_input, currency_output) do
     if currency_input == currency_output do
       :error
