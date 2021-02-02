c6c2cbefdebc6231fa80ab83fb83dd63140c642b
klinsmann lopes <lopesklismann@gmail.com>
2021-02-02 18:00:34 -0300

    Changing responsibility

--- lib/account_simulator/cli/login/login.ex
+++ lib/account_simulator/cli/login/login.ex
@@ -6,13 +6,13 @@ defmodule AccountSimulator.CLI.Login.Login do
   alias AccountSimulator.Mix.Tasks.Utils.PromptHelper
   alias AccountSimulator.CLI.Login.AccountConsult
   alias AccountSimulator.CLI.Menu.ChoiceTransactions
-  alias AccountSimulator.CLI.Currency
+  alias AccountSimulator.CLI.Login.Account
 
   # Aqui passamos para função de acesso o dado de entrada do usuário e uma estrutura de dados default.
   def login do
     PromptHelper.prompt_message("Digite seu nome de usuário: ")
     |> PromptHelper.string_atom()
-    |> access(standard_user())
+    |> access(Account.standard_user())
   end
 
   # Função que pega uma estrutura de dados, e na mesma realiza uma pesquisa na estrutura com o dado de entrada do usuário.
@@ -27,10 +27,5 @@ defmodule AccountSimulator.CLI.Login.Login do
       _ ->
         ChoiceTransactions.option_transactions(usuarios, usuario)
     end
-  end
-
-  # Estrutura de dados default.
-  def standard_user do
-    [francisca: Currency.new(), stone: Currency.new(), klinsmann: Currency.new(), lopes: Currency.new(), paula: Currency.new()]
   end
 end
