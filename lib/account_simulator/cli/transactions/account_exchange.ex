b533b5358ca67ecc4a775d3cbfc9b722a2431189
klinsmann lopes <lopesklismann@gmail.com>
2021-02-02 01:44:49 -0300

    Performing exchange and calculating weight

--- lib/account_simulator/cli/transactions/account_exchange.ex
+++ lib/account_simulator/cli/transactions/account_exchange.ex
@@ -2,6 +2,7 @@ defmodule AccountSimulator.Mix.CLI.Trasactions.AccountExchange do
   alias Mix.Shell.IO, as: Shell
   alias AccountSimulator.Mix.CLI.Errors.Exception
   alias AccountSimulator.Mix.CLI.Trasactions.AccountTransactions
+  alias AccountSimulator.CLI.Currency
 
   # Removendo dinheiro de uma moeda específica.
   def remove_currency(users, user, currency, value) do
@@ -26,7 +27,17 @@ defmodule AccountSimulator.Mix.CLI.Trasactions.AccountExchange do
       Shell.prompt("Não é possível realizar câmbio para a mesma moeda. Digite as moedas novamente, aperte ENTER para continuar.")
       currency_change(users, user)
     end
-    
+    new_data_users = currency_exchange(users, user, currency_input, currency_output, value)
+    Shell.cmd("clear")
+    Shell.info("Foi realizado o câmbio entre as moedas #{currency_input} => #{currency_output}")
+    Shell.prompt("Aperte ENTER para voltar ao menu de transações")
+    new_data_users
+  end
+
+  def currency_exchange(users, user, currency_input, currency_output, value) do
+    weight_input = get_weight(currency_input)
+    weight_output = get_weight(currency_output)
+    add_currency(remove_currency(users, user, currency_input, value), user, currency_output, calculation_exchange(rate(weight_input, weight_output), value))
   end
 
   # Verifica saldo e pega moeda digitada.
@@ -55,6 +66,16 @@ defmodule AccountSimulator.Mix.CLI.Trasactions.AccountExchange do
 
   # Obtem o peso da moeda
   def get_weight(currency) do
-    Moeda.info()[currency].weight
+    Currency.info()[currency].weight
+  end
+
+  # Divide os pesos das moedas.
+  def rate(weight_input, weight_output) do
+    weight_input / weight_output 
+  end
+
+  # Calcula o valor a ser depositado.
+  def calculation_exchange(rate, value) do
+    trunc(rate * value)
   end
 end
