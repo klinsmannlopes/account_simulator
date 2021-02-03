107bf05b19122f3f0adfddb62b8a4e449afee066
klinsmann lopes <lopesklismann@gmail.com>
2021-02-03 07:33:23 -0300

    Refactor name variables

--- lib/account_simulator/cli/login/login.ex
+++ lib/account_simulator/cli/login/login.ex
@@ -16,16 +16,15 @@ defmodule AccountSimulator.CLI.Login.Login do
   end
 
   # Função que pega uma estrutura de dados, e na mesma realiza uma pesquisa na estrutura com o dado de entrada do usuário.
-  def access(usuario, usuarios) do
-    case AccountConsult.get_user?(usuario, usuarios) do
+  def access(user, users) do
+    case AccountConsult.get_user?(user, users) do
       :error ->
         Shell.cmd("clear")
         Shell.error("Usuário não existe!")
         Shell.prompt("Pressione ENTER para voltar a tela de login...")
         AccountSimulator.CLI.Menu.ChoiceLogin.start()
-
       _ ->
-        ChoiceTransactions.option_transactions(usuarios, usuario)
+        ChoiceTransactions.option_transactions(users, user)
     end
   end
 end
